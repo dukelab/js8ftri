0ed4671eb19f183b633779efb7e3a23002c63f49
dukelab <jeongman7@hotmail.com>
2014-12-22 00:37:28 +0900

    Exercises18

---
+++ src/main/java/dukelab/js8ftri/ch3/Exercises18.java
@@ -0,0 +1,30 @@
+package dukelab.js8ftri.ch3;
+
+import java.io.FileInputStream;
+import java.util.function.Function;
+
+public class Exercises18 {
+
+    public static void main(String[] args) {
+        System.out.println(unchecked(t -> t).apply("OK"));
+        System.out.println(unchecked(t -> new FileInputStream("..")).apply("fff"));
+    }
+
+    public static <T, U> Function<T, U> unchecked(ThrowableFunction<T, U> f) {
+        return (t) -> {
+            try {
+                return f.apply(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } catch (Throwable th) {
+                throw th;
+            }
+        };
+    }
+
+    @FunctionalInterface
+    public static interface ThrowableFunction<T, U> {
+        U apply(T t) throws Exception;
+    }
+
+}
