9b37761906d2d8a0db30023c79d345d3c29d75c4
dukelab <jeongman7@hotmail.com>
2014-12-26 01:30:32 +0900

    Chapter 5 Exercise1

---
+++ src/main/java/dukelab/js8ftri/ch5/Exercise1.java
@@ -0,0 +1,18 @@
+package dukelab.js8ftri.ch5;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exercise1 {
+
+    @Test
+    public void test() {
+        LocalDate expected = LocalDate.of(2014, 9, 13);
+        LocalDate actual = LocalDate.ofYearDay(2014, 256);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
