31345bfc3fa3ce938743d8eee26b151cf8a9730c
dukelab <jeongman7@hotmail.com>
2014-12-31 13:49:28 +0900

    Exercise5

---
+++ src/main/java/dukelab/js8ftri/ch5/Exercise5.java
@@ -0,0 +1,24 @@
+package dukelab.js8ftri.ch5;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class Exercise5 {
+
+    @Test
+    public void test() {
+        // is not my birthday... fake
+        LocalDate myBirthday = LocalDate.of(1999, 12, 31);
+        long actual = myBirthday.until(LocalDate.of(2014, 12, 31), ChronoUnit.DAYS);
+
+        // 2000, 2004, 2008, 2012 are leap years.
+        long expected = (365 * 11) + (366 * 4);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
