2e01cfbfc8ccdc0a18a2e837db87c87596a1f230
dukelab <jeongman7@hotmail.com>
2014-12-31 14:24:57 +0900

    Exercise6

---
+++ src/main/java/dukelab/js8ftri/ch5/Exercise6.java
@@ -0,0 +1,22 @@
+package dukelab.js8ftri.ch5;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class Exercise6 {
+
+    @Test
+    public void test() {
+        LocalDate next13Friday = LocalDate.of(1900, 1, 13);
+        LocalDate to = LocalDate.of(1999, 12, 31);
+        while (!next13Friday.isAfter(to)) {
+            if (next13Friday.getDayOfWeek() == DayOfWeek.FRIDAY) {
+                System.out.println(next13Friday);
+            }
+            next13Friday = next13Friday.plusMonths(1);
+        }
+    }
+
+}
