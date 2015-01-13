22dd5f17f05302fc394ccf865b9bb9b7c0985522
dukelab <jeongman7@hotmail.com>
2015-01-13 17:57:18 +0900

    Chapter6 Exercise1

---
+++ src/main/java/dukelab/js8ftri/ch6/Exercise1.java
@@ -0,0 +1,39 @@
+package dukelab.js8ftri.ch6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.LongAccumulator;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+public class Exercise1 {
+
+    @Test
+    public void test() throws InterruptedException {
+        AtomicReference<String> longest = new AtomicReference<>();
+        LongAccumulator accumulator = new LongAccumulator(Math::max, 0);
+        prepareWords().parallelStream()
+            .forEach(observed -> {
+                System.out.println("Current working thread : " + Thread.currentThread() + ", observed : " + observed);
+                longest.updateAndGet(current -> {
+                    String result = observed.length() > accumulator.intValue() ? observed : current;
+                    accumulator.accumulate(observed.length());
+                    return result;
+                });
+            });
+
+        System.out.println("LongestWord : " + longest.get());
+    }
+
+    private List<String> prepareWords() {
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            words.add(RandomStringUtils.randomAlphabetic(RandomUtils.nextInt(5, 50)));
+        }
+        return words;
+    }
+
+}
